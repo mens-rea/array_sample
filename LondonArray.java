f0e23d087644ed2ed952d522ab90fd0087b49064
lesterlondon <lesterlondon0489@gmail.com>
2018-08-02 02:08:02 +0800

    add own array

---
+++ LondonArray.java
@@ -0,0 +1,12 @@
+public class LondonArray{
+    public static void main(String args[]){
+
+        // declaring cars array
+        String cars[] = {"accent", "honda", "toyota", "nissan", "mazda", "subaru"};
+
+        // display all cars
+        for(int x=0; x<cars.length; x++){
+            System.out.println(cars[x]);
+        }
+    }
+}
