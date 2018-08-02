f775c1daf675c7bb540d02c03f7d9d03d8332828
Juan Miguel Alvarez <jm.alvarez@descouvre.com>
2018-08-02 08:56:30 +0800

    first commit

---
+++ ArraySample.java
@@ -0,0 +1,11 @@
+public class ArraySample{
+    
+    public static void main(String args[]){
+        String fruits[] = {"apple", "mango", "banana", "watermelon", "strawberry"};
+    
+        for(int x = 0; x<fruits.length; x++){
+            System.out.println(fruits[x]);
+        }
+    }
+
+}
