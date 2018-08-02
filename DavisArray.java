ab18dbd0cb5dabb8eb756c24e90212491f6ece1f
Rizza Michelle Davis <rizzamichelle.davis@gmail.com>
2018-08-02 12:58:56 +0800

    add own array

---
+++ DavisArray.java
@@ -0,0 +1,15 @@
+public class DavisArray{
+    public static void main(String args[]){
+
+        //declaring dogs array
+        String dogs[]={"shih tzu", "japanese spitz", "belgian malinois", "pitbull terrier", "saint bernard"};
+
+        //display all dogs
+        for(int x = 0; x<dogs.length; x++){
+            System.out.println(dogs[x]);    
+
+        }
+        
+    }
+
+}
