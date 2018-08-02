4627b6c3775b8b296bd71d03a8d53f7c7984174e
Juan Miguel Alvarez <jm.alvarez@descouvre.com>
2018-08-02 09:13:37 +0800

    add first activity

---
+++ ArrayStoryActivity.java
@@ -0,0 +1,11 @@
+public class ArrayStoryActivity{
+    
+    public static void main(String args[]){
+        String fruits[] = {"Black", "You", "Are", "An", "Ship", "Excellent", "Can", "Save", "Programmer", "Two", "Wolf", "Sheep", "Sea"};
+    
+        for(int x = 0; x<fruits.length; x++){
+            System.out.println(fruits[x]);
+        }
+    }
+
+}
